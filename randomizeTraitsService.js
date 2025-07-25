4378cd8c5d6022b05a3e2a3f804bc50dbcc15930
pm-white <philmwhite@pm.me>
2025-07-25 08:52:22 -0400

    complete working version

--- randomizeTraitsService.js
+++ randomizeTraitsService.js
@@ -7,25 +7,50 @@ const port = 10000;
 app.use(express.json());
 
 app.post("/randomize-traits", (req, res) => {
-  const data = req.body.traits;
+  const data = req.body;
 
-  // destructure locked traits arr and traits
-  const { lockedTraits, ...traits } = data;
+  const { lockedTraits, classes, subclasses, species, origins, alignments } =
+    data;
+
+  const selectedClass = random(0, classes - 1);
+  const selectedSubclass = random(0, subclasses[selectedClass] - 1);
+  const selectedSpecies = random(0, species - 1);
+  const selectedOrigin = random(0, origins - 1);
+  const selectedAlignment = random(0, alignments - 1);
+
+  const randomTraits = {
+    classes: selectedClass,
+    subclasses: selectedSubclass,
+    species: selectedSpecies,
+    origins: selectedOrigin,
+    alignments: selectedAlignment,
+  };
 
   // remove locked traits
-  const usableTraits = omit(traits, lockedTraits);
+  const usableTraits = omit(randomTraits, lockedTraits);
 
-  // initialize return traits object
-  // NOTE: doesn't handle sublcasses
-  let randomTraits = new Object();
-  for (const key of keys(usableTraits)) {
-    randomTraits[key] = random(0, usableTraits[key] - 1);
+  // fix cases
+  const returnTraits = {};
+  const useableTraitsKeys = keys(usableTraits);
+  for (const key of useableTraitsKeys) {
+    if (key === "classes") {
+      returnTraits["class"] = usableTraits[key];
+    }
+    if (key === "subclasses") {
+      returnTraits["subclass"] = usableTraits[key];
+    }
+    if (key === "species") {
+      returnTraits["species"] = usableTraits[key];
+    }
+    if (key === "origins") {
+      returnTraits["origin"] = usableTraits[key];
+    }
+    if (key === "alignments") {
+      returnTraits["alignment"] = usableTraits[key];
+    }
   }
 
-  console.log("usableTraits:", usableTraits);
-  console.log("randomTraits:", randomTraits);
-
-  res.json(traits);
+  res.json(returnTraits);
 });
 
 app.listen(port, () => {

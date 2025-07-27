377f3855f8e7e81b51de6c1fb5586cfd4465fa09
pm-white <philmwhite@pm.me>
2025-07-27 16:42:13 -0400

    create test call program

---
+++ testCall.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const response = await fetch("http://localhost:10000/randomize-traits", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      lockedTraits: ["origins"],
+      classes: 10,
+      subclasses: [3, 3, 4, 5, 2, 6, 3, 3, 4, 5],
+      species: 9,
+    }),
+  });
+
+  const data = await response.json();
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Test call service listening on port ${port}`);
+});
